3ced8848f6ccfe329ec2b4edabcfcb9d27dbd23c
Michael Bond <codemonkey8510@icloud.com>
2022-05-01 14:54:32 -0400

    Some fixes

--- CloneAllRepos/Program.cs
+++ CloneAllRepos/Program.cs
@@ -1,50 +1,48 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Octokit;
 using System.Diagnostics;
-using System.Text;
 using ProductHeaderValue = Octokit.ProductHeaderValue;
 
-IConfiguration config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddUserSecrets(typeof(Program).Assembly)
-    .AddEnvironmentVariables()
-    .AddCommandLine(args)
-    .Build();
-
-var targetDirectory = config?.GetValue<string>("targetDirectory") ?? string.Empty;
-var githubUserName = config?.GetValue<string>("githubUserName") ?? string.Empty;
-var personalAccessToken = config?.GetValue<string>("personalAccessToken") ?? string.Empty;
-
 IList<string> fails = new List<string>();
-
-var myUser = new ProductHeaderValue(githubUserName);
-var credentials = new Credentials(personalAccessToken);
-var client = new GitHubClient(myUser) { Credentials = credentials };
-
-var repos = await client.Repository.GetAllForCurrent();
-
-CloneRepos(targetDirectory, repos);
-if (fails.Count > 0)
+try
 {
-    Console.WriteLine($"{Environment.NewLine}Fails:{Environment.NewLine}");
-    foreach (var fail in fails)
+    IConfiguration config = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddUserSecrets(typeof(Program).Assembly)
+        .AddEnvironmentVariables()
+        .AddCommandLine(args)
+        .Build();
+
+    var targetDirectory = config.GetValue("targetDirectory", string.Empty);
+    var githubUserName = config.GetValue("githubUserName", string.Empty);
+    var personalAccessToken = config.GetValue("personalAccessToken", string.Empty);
+
+    var myUser = new ProductHeaderValue(githubUserName);
+    var credentials = new Credentials(personalAccessToken);
+    var client = new GitHubClient(myUser) { Credentials = credentials };
+
+    var repos = await client.Repository.GetAllForCurrent();
+
+    CloneRepos(targetDirectory, repos);
+    if (fails.Count > 0)
     {
-        Console.WriteLine(fail);
+        Console.WriteLine($"{Environment.NewLine}Fails:{Environment.NewLine}");
+        foreach (var fail in fails)
+        {
+            Console.WriteLine(fail);
+        }
     }
+}
+catch (Exception ex)
+{
+    LogExceptions(ex);
 }
 
 void CloneRepos(string targetDirectory, IEnumerable<Repository> repos)
 {
-    try
+    foreach (var repo in repos)
     {
-        foreach (var repo in repos)
-        {
-            CloneRepo(targetDirectory, repo);
-        }
-    }
-    catch (Exception ex)
-    {
-        LogExceptions(ex);
+        CloneRepo(targetDirectory, repo);
     }
 }
 
@@ -100,16 +98,9 @@ void CloneRepo(string targetDirectory, Repository repo)
 
 void LogExceptions(Exception ex)
 {
-    var sbError = new StringBuilder();
-    AddExceptionToLog(sbError, ex);
-    Console.WriteLine(sbError);
-}
-
-void AddExceptionToLog(StringBuilder sbError, Exception ex)
-{
     Console.WriteLine($"{ex.Message}{Environment.NewLine}{ex.StackTrace}");
     if (ex.InnerException is not null)
     {
-        AddExceptionToLog(sbError, ex.InnerException);
+        LogExceptions(ex.InnerException);
     }
 }
