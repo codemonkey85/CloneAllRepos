41093c431c8e00d2358a4e19e08a45ad4dff29bc
Michael Bond <codemonkey8510@icloud.com>
2022-06-30 14:30:25 -0400

    A bit of cleanup

--- CloneAllRepos/Program.cs
+++ CloneAllRepos/Program.cs
@@ -2,21 +2,20 @@
 using Octokit;
 using System.Diagnostics;
 using System.Text;
-using ProductHeaderValue = Octokit.ProductHeaderValue;
 
 IList<string> fails = new List<string>();
-var targetDirectory = string.Empty;
+var targetReposDirectory = string.Empty;
 
 try
 {
     IConfiguration config = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile("appsettings.json", true, true)
         .AddUserSecrets(typeof(Program).Assembly)
         .AddEnvironmentVariables()
         .AddCommandLine(args)
         .Build();
 
-    targetDirectory = config.GetValue("targetDirectory", string.Empty);
+    targetReposDirectory = config.GetValue("targetDirectory", string.Empty);
     var githubUserName = config.GetValue("githubUserName", string.Empty);
     var personalAccessToken = config.GetValue("personalAccessToken", string.Empty);
 
@@ -32,12 +31,15 @@ try
         {
             var fork = await client.Repository.Get(githubUserName, repo.Name);
             var upstream = fork.Parent;
-            var compareResult = await client.Repository.Commit.Compare(upstream.Owner.Login, upstream.Name, upstream.DefaultBranch, $"{fork.Owner.Login}:{fork.DefaultBranch}").ConfigureAwait(false);
+            var compareResult = await client.Repository.Commit.Compare(upstream.Owner.Login, upstream.Name,
+                upstream.DefaultBranch, $"{fork.Owner.Login}:{fork.DefaultBranch}").ConfigureAwait(false);
             if (compareResult.BehindBy > 0)
             {
                 Console.WriteLine($"Updating fork of {repo.Name}");
-                var upstreamBranchReference = await client.Git.Reference.Get(upstream.Owner.Login, upstream.Name, $"heads/{upstream.DefaultBranch}").ConfigureAwait(false);
-                await client.Git.Reference.Update(fork.Owner.Login, fork.Name, $"heads/{fork.DefaultBranch}", new ReferenceUpdate(upstreamBranchReference.Object.Sha)).ConfigureAwait(false);
+                var upstreamBranchReference = await client.Git.Reference
+                    .Get(upstream.Owner.Login, upstream.Name, $"heads/{upstream.DefaultBranch}").ConfigureAwait(false);
+                await client.Git.Reference.Update(fork.Owner.Login, fork.Name, $"heads/{fork.DefaultBranch}",
+                    new ReferenceUpdate(upstreamBranchReference.Object.Sha)).ConfigureAwait(false);
             }
         }
         catch (Exception ex)
@@ -47,28 +49,24 @@ try
         }
     }
 
-    var allDirs = Directory.GetDirectories(targetDirectory).Select(dir => new DirectoryInfo(dir).Name);
+    var allDirs = Directory.GetDirectories(targetReposDirectory).Select(dir => new DirectoryInfo(dir).Name);
     var repoDirs = repos.Select(repo => repo.Name);
 
     var remainingDirs = allDirs.Where(dir => !repoDirs.Contains(dir, StringComparer.OrdinalIgnoreCase));
 
     foreach (var repo in repos)
     {
-        CloneOrUpdateRepo(targetDirectory, repo, client);
+        CloneOrUpdateRepo(targetReposDirectory, repo);
     }
 
     foreach (var repo in remainingDirs)
     {
-        var destinationPath = Path.Combine(targetDirectory, repo);
-        var process = Process.Start(new ProcessStartInfo
+        var destinationPath = Path.Combine(targetReposDirectory, repo);
+        Process.Start(new ProcessStartInfo
         {
-            WorkingDirectory = destinationPath,
-            FileName = "git",
-            Arguments = $"pull",
-            CreateNoWindow = false,
+            WorkingDirectory = destinationPath, FileName = "git", Arguments = "pull", CreateNoWindow = false
         });
     }
-
 }
 catch (Exception ex)
 {
@@ -76,7 +74,7 @@ catch (Exception ex)
 }
 finally
 {
-    if (fails.Count > 0 && !string.IsNullOrEmpty(targetDirectory))
+    if (fails.Count > 0 && !string.IsNullOrEmpty(targetReposDirectory))
     {
         var sbFails = new StringBuilder();
         sbFails.AppendLine($"{Environment.NewLine}Fails:{Environment.NewLine}");
@@ -84,12 +82,13 @@ finally
         {
             sbFails.AppendLine(fail);
         }
+
         Console.WriteLine(sbFails);
-        File.WriteAllText(Path.Combine(targetDirectory, "log.txt"), sbFails.ToString());
+        File.WriteAllText(Path.Combine(targetReposDirectory, "log.txt"), sbFails.ToString());
     }
 }
 
-void CloneOrUpdateRepo(string targetDirectory, Repository repo, GitHubClient gitHubClient)
+void CloneOrUpdateRepo(string targetDirectory, Repository repo)
 {
     try
     {
@@ -102,20 +101,16 @@ void CloneOrUpdateRepo(string targetDirectory, Repository repo, GitHubClient git
                 WorkingDirectory = targetDirectory,
                 FileName = "git",
                 Arguments = $"clone {repo.SshUrl} --no-tags",
-                CreateNoWindow = true,
+                CreateNoWindow = true
             });
             if (process is null)
             {
                 throw new Exception("Cannot create process");
             }
-            if (process.WaitForExit(1000 * 30))
-            {
-                Console.WriteLine($"Repo {repo.Name} finished cloning");
-            }
-            else
-            {
-                Console.WriteLine($"Repo {repo.Name} did not finish cloning");
-            }
+
+            Console.WriteLine(process.WaitForExit(1000 * 30)
+                ? $"Repo {repo.Name} finished cloning"
+                : $"Repo {repo.Name} did not finish cloning");
             var path = Path.Combine(targetDirectory, repo.Name);
             if (!Directory.Exists(path))
             {
@@ -125,12 +120,9 @@ void CloneOrUpdateRepo(string targetDirectory, Repository repo, GitHubClient git
         else
         {
             Console.WriteLine($"Updating {repo.Name}");
-            var process = Process.Start(new ProcessStartInfo
+            Process.Start(new ProcessStartInfo
             {
-                WorkingDirectory = destinationPath,
-                FileName = "git",
-                Arguments = $"pull",
-                CreateNoWindow = false,
+                WorkingDirectory = destinationPath, FileName = "git", Arguments = "pull", CreateNoWindow = false
             });
         }
     }
@@ -142,11 +134,17 @@ void CloneOrUpdateRepo(string targetDirectory, Repository repo, GitHubClient git
 
 void LogExceptions(Exception ex)
 {
-    var errorLine = $"{ex.Message}{Environment.NewLine}{ex.StackTrace}";
-    Console.Error.WriteLine(errorLine);
-    fails.Add(errorLine);
-    if (ex.InnerException is not null)
+    while (true)
     {
-        LogExceptions(ex.InnerException);
+        var errorLine = $"{ex.Message}{Environment.NewLine}{ex.StackTrace}";
+        Console.Error.WriteLine(errorLine);
+        fails.Add(errorLine);
+        if (ex.InnerException is not null)
+        {
+            ex = ex.InnerException;
+            continue;
+        }
+
+        break;
     }
 }
